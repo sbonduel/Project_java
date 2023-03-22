07a8d4c80288d5660b7484fe0baf8d37eeda2214
unknown <damien.coulom@gmail.com>
2023-03-22 09:05:17 +0100

    Commit Serialisation début

---
+++ SerialisationCSV/src/SeriaCSV/SerialisationCSV.java
@@ -0,0 +1,25 @@
+package SeriaCSV;
+
+import java.util.Date;
+
+public class SerialisationCSV {
+	private String nom;
+    private String prenom;
+    private String hobbit;
+    private String idImage;
+    private Date anniversaire;
+    private int age;
+   // private Adresse adresse;
+    private String numTel;
+    private String email;
+    private String sexe;
+    private String attiranceSexuelle;
+    private int ageMinPref;
+    private int ageMaxPref;
+    private String presentationCommentaire; 
+
+	public SerialisationCSV() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
