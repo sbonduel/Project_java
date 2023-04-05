9e1fd481aa6309e05de7805cefe427c8513c8117
unknown <damien.coulom@gmail.com>
2023-04-05 09:54:47 +0200

    Update SerialisationCSV + fichier de test

--- SerialisationCSV/src/SeriaCSV/SerialisationCSV.java
+++ SerialisationCSV/src/SeriaCSV/SerialisationCSV.java
@@ -1,10 +1,14 @@
+//package Test;
 package SeriaCSV;
+//Type date à local date à changer
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
+
 
 public class SerialisationCSV {
 	private String nom;
@@ -41,10 +45,10 @@ public class SerialisationCSV {
     
     private String presentationCommentaire; 
 
-	public SerialisationCSV() {
+
 		
 		//Constructeur avec des ints
-		SerialisationCSV(String name,String sirname,String Hobbys,String identifImage,Date BirthDayDate,int AgePers,String Ville,String RueNum,int CodePost,String Region,String Country,String NumeroDeTel,String Email,String Sexe,String Attirance,int AgeMin,int AgeMax,string Commentary)
+	public	SerialisationCSV(String name,String sirname,String Hobbys,String identifImage,Date BirthDayDate,int AgePers,String Ville,String RueNum,int CodePost,String Region,String Country,String NumeroDeTel,String Email,String Sexe,String Attirance,int AgeMin,int AgeMax,String Commentary)
 		{
 		 nom=name;
 		 prenom=sirname;
@@ -61,7 +65,7 @@ public class SerialisationCSV {
 			//String Heures=DateF.substring(11, Math.min(DateF.length(), 19));
 			 Annee=DateF.substring(24, Math.min(DateF.length(), 28));
 		    
-		    private int age = AgePers;
+		   age = AgePers;
 		    
 		    ville=Ville;
 		    rueNumero=RueNum;
@@ -80,7 +84,7 @@ public class SerialisationCSV {
 		 AgeMinimumPreference = Integer.toString(ageMinPref);
 
 		   ageMaxPref = AgeMax;
-		AgeMaximumPreference = Integer.toString(ageMaxPref)
+		AgeMaximumPreference = Integer.toString(ageMaxPref);
 
 		    
 		    presentationCommentaire = Commentary;	
@@ -103,7 +107,7 @@ public class SerialisationCSV {
 			//Ecriture des données selon le format correspondant
 			
 			PrintWriter writer = new PrintWriter(CheminG+"/Data.csv", "UTF-8");
-			writer.println("Hello world !");
+			writer.println(nom+","+prenom+","+hobbit+","+idImage+","+Jour+"/"+Mois+"/"+Annee+","+Age+","+ville+","+rueNumero+","+CodePostal+","+region+","+pays+","+numTel+","+email+","+sexe+","+attiranceSexuelle+","+AgeMinimumPreference+","+AgeMaximumPreference+","+presentationCommentaire+",");
 			
 			
 			//writer.println("Test "+Sujet);
@@ -136,11 +140,23 @@ public class SerialisationCSV {
 		
 		
 		
-		public void AddToCsvPointVirgule()
+		public void AddToCsvPointVirgule() throws FileNotFoundException, UnsupportedEncodingException
 		{
+			String CheminG=System.getProperty("user.dir");
+			String old ="\\";
+			String mnew="/";
+			CheminG=CheminG.replace(old,mnew);
+			File fichier = new File(CheminG,"Data2.csv");
+			//Création du fichier de donnés txt dans le chemin actuel
+			//File fichier = new File(CheminG,"Data.csv");
+			//Ecriture des données selon le format correspondant
+			
+			PrintWriter writer =  new PrintWriter(CheminG+"/Data2.csv", "UTF-8");
+			writer.println(nom+";"+prenom+";"+hobbit+";"+idImage+";"+Jour+"/"+Mois+"/"+Annee+";"+Age+";"+ville+";"+rueNumero+";"+CodePostal+";"+region+";"+pays+";"+numTel+";"+email+";"+sexe+";"+attiranceSexuelle+";"+AgeMinimumPreference+";"+AgeMaximumPreference+";"+presentationCommentaire+";");
+			writer.close();
 			
 		}
 
-	}
+	
 
 }
--- SerialisationCSV/src/SeriaCSV/TestCsv.java
+++ SerialisationCSV/src/SeriaCSV/TestCsv.java
@@ -1,15 +1,38 @@
 package SeriaCsv;
 
-public class TestCsv {
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
-	public TestCsv() {
+public class TestClasse {
+
+	public TestClasse() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     
-		
+		java.util.Date date = new java.util.Date(); 
+		SerialisationCSV monTest = new SerialisationCSV(null, null, null, null, date, 0, null, null, 0, null, null, null, null, null, null, 0, 0, null);
+        //monTest.AddToCsvPointVirgule();
+        try {
+			monTest.AddToCsvVirgule();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+		monTest.AddToCsvPointVirgule();
+		} catch (FileNotFoundException e) {
+		 
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			
+			e.printStackTrace();
+		}
+        monTest.ObtenirChemin();
+        System.out.println(monTest.ObtenirChemin());
 	}
 
 }
+
