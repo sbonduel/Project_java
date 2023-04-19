85c1f84e16b93f77c80b964f55138a32919b46ed
sbonduel <bonduelle.simon@gmail.com>
2023-04-19 17:47:08 +0200

    avancement

--- SerialisationCSV/src/project_Vue/Fenetre.java
+++ SerialisationCSV/src/project_Vue/Fenetre.java
@@ -7,9 +7,10 @@ public class Fenetre extends JFrame {
     public Panel_recherche recherche;
     public ConnexionPanel connexion_panel;
     private Menu menu;
+    private inscription s_incrire;
 
     public Fenetre() {
-        super("Ma Fenetre");
+        super("Champo_Love");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 300);
         setLocationRelativeTo(null); // Centrer la fenêtre sur l'écran
@@ -17,7 +18,8 @@ public class Fenetre extends JFrame {
         recherche = new Panel_recherche();
         menu = new Menu();
         connexion_panel = new ConnexionPanel();
-        add(Box.createRigidArea(new Dimension(200, 510))); // Ajouter un espace rigide pour déplacer le panneau de connexion vers le centre
+        s_incrire = new inscription();
+        add(Box.createRigidArea(new Dimension(0, 510))); // Ajouter un espace rigide pour déplacer le panneau de connexion vers le centre
         
         //pack(); // Ajuster la taille de la fenêtre pour qu'elle s'adapte à son contenu
     }
@@ -30,13 +32,22 @@ public class Fenetre extends JFrame {
     		f.add(f.connexion_panel, BorderLayout.CENTER);
     	}
         if (panel==1) {
+        	f.setSize(1100, 600); 
         	f.add(f.recherche, BorderLayout.NORTH);
         	f.setJMenuBar(f.menu.getMenuBar());
             f.menu.getMenu().add(new JMenuItem("déconectez"));
             f.menu.getMenu().add(new JMenuItem("S'inscrire"));
-        	f.add(f.connexion_panel, BorderLayout.CENTER);
+        	f.add(f.s_incrire, BorderLayout.CENTER);
         }
-        
+        if (panel==2) {
+        	f.setSize(1100, 600); 
+        	f.add(f.recherche, BorderLayout.NORTH);
+        	f.setJMenuBar(f.menu.getMenuBar());
+            f.menu.getMenu().add(new JMenuItem("déconectez"));
+            f.menu.getMenu().add(new JMenuItem("S'inscrire"));
+            
+        	f.add(f.s_incrire, BorderLayout.CENTER);
+        }
     	
     	
 
---
+++ SerialisationCSV/src/project_Vue/inscription.java
@@ -0,0 +1,159 @@
+package project_Vue;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class inscription extends JPanel {
+
+    
+	
+	public inscription() {
+		setLayout(new BorderLayout()); // Utiliser un BorderLayout pour ce panneau
+		JPanel inscriptions = new JPanel(new GridLayout(0, 1));
+    //
+    JPanel nomprenom = new JPanel();
+    JLabel nom= new JLabel("nom :");
+    JTextField nom_1 = new JTextField();
+    JLabel prenom= new JLabel("prénom :");
+    JTextField prenom_1 = new JTextField();
+
+    
+    //
+    JPanel age_sexe_email = new JPanel();
+    JLabel age = new JLabel("âge :");
+    JTextField age_1 = new JTextField();
+    
+    JLabel sex= new JLabel("sex :");
+    JTextField sex_1 = new JTextField();
+    JLabel email= new JLabel("email :");
+    JTextField email_1 = new JTextField();
+    
+    
+    JPanel hobbits = new JPanel();
+    JLabel hobbit = new JLabel("hobbit :");
+    JTextField hobbit_1 = new JTextField();
+
+    JPanel image = new JPanel();
+    JLabel idImage= new JLabel("nom de votre image :");
+    JTextField idImage_1 = new JTextField();
+
+    JPanel orientation_sex_numTel = new JPanel();
+    JLabel orentation_sexuelle= new JLabel("orentation sexuelle :");
+    JTextField orentation_sexuelle_1 = new JTextField();
+    JLabel numTel = new JLabel("votre numéro des teléphone :");
+    JTextField numTel_1 = new JTextField();
+
+    JPanel presentation_Commentaire = new JPanel();
+    JLabel presentationCommentaire= new JLabel("voulez-vous renseigner d'autres informations qui vous semblent importantes :");
+    JTextField presentationCommentaire_1 = new JTextField();
+
+    JPanel agepref = new JPanel();
+    JLabel agepreference= new JLabel("âge minimum et maximum :");
+    JLabel agemin= new JLabel("âge minimum :");
+    JTextField agemin_1 = new JTextField();
+    JLabel agemax= new JLabel("âge maximum :");
+    JTextField agemax_1 = new JTextField();
+
+    JPanel adresse = new JPanel();
+    JLabel adresses = new JLabel("adresse :");
+    JLabel Ville = new JLabel("ville :");
+    JTextField Ville_1 = new JTextField();
+    JLabel rue  = new JLabel("rue :");
+    JTextField rue_1 = new JTextField();
+    JLabel numrue  = new JLabel("numéro de rue :");
+    JTextField numrue_1 = new JTextField();
+    JLabel codePostal = new JLabel("code postal :");
+    JTextField codePostal_1 = new JTextField();
+    JLabel region = new JLabel("région :");
+    JTextField region_1 = new JTextField();
+    JLabel pays = new JLabel("pays :");
+    JTextField pays_1 = new JTextField();
+
+    JPanel aniv = new JPanel();
+    JLabel naissance = new JLabel("née le :");
+    JLabel jours = new JLabel("jour :");
+    JTextField jours_1 = new JTextField();
+    JLabel mois = new JLabel("mois :");
+    JTextField mois_1 = new JTextField();
+    JLabel anees = new JLabel("année :");
+    JTextField anees_1 = new JTextField();
+
+    hobbits.add(hobbit);
+    hobbits.add(hobbit_1);
+    inscriptions.add(hobbits);
+
+    image.add(idImage);
+    image.add(idImage_1);
+    inscriptions.add(image);
+
+    orientation_sex_numTel.add(orentation_sexuelle);
+    orientation_sex_numTel.add(orentation_sexuelle_1);
+    orientation_sex_numTel.add(numTel);
+    orientation_sex_numTel.add(numTel_1);
+    inscriptions.add(orientation_sex_numTel);
+
+    presentation_Commentaire.add(presentationCommentaire);
+    presentation_Commentaire.add(presentationCommentaire_1);
+    inscriptions.add(presentation_Commentaire);
+
+    agepref.add(agepreference);
+    agepref.add(agemin);
+    agepref.add(agemin_1);
+    agepref.add(agemax);
+    agepref.add(agemax_1);
+    inscriptions.add(agepref);
+
+    adresse.add(adresses);
+    adresse.add(Ville);
+    adresse.add(Ville_1);
+    adresse.add(rue);
+    adresse.add(rue_1);
+    adresse.add(numrue);
+    adresse.add(numrue_1);
+    adresse.add(codePostal);
+    adresse.add(codePostal_1);
+    adresse.add(region);
+    adresse.add(region_1);
+    adresse.add(pays);
+    adresse.add(pays_1);
+    inscriptions.add(adresse);
+
+    aniv.add(naissance);
+    aniv.add(jours);
+    aniv.add(jours_1);
+    aniv.add(mois);
+    aniv.add(mois_1);
+    aniv.add(anees);
+    aniv.add(anees_1);
+    inscriptions.add(aniv);
+    
+    
+
+    // continue
+    add(inscriptions, BorderLayout.CENTER);
+    int x = 90;
+    int y = 20;
+    nom_1.setPreferredSize(new Dimension(x, y));
+    prenom_1.setPreferredSize(new Dimension(x, y));
+    age_1.setPreferredSize(new Dimension(x, y));
+    sex_1.setPreferredSize(new Dimension(x, y));
+    email_1.setPreferredSize(new Dimension(x, y));
+    hobbit_1.setPreferredSize(new Dimension(x, y));
+    idImage_1.setPreferredSize(new Dimension(x, y));
+    orentation_sexuelle_1.setPreferredSize(new Dimension(x, y));
+    numTel_1.setPreferredSize(new Dimension(x, y));
+    presentationCommentaire_1.setPreferredSize(new Dimension(500, y));
+    agemin_1.setPreferredSize(new Dimension(x, y));
+    agemax_1.setPreferredSize(new Dimension(x, y));
+    Ville_1.setPreferredSize(new Dimension(x, y));
+    rue_1.setPreferredSize(new Dimension(x, y));
+    numrue_1.setPreferredSize(new Dimension(x, y));
+    codePostal_1.setPreferredSize(new Dimension(x, y));
+    region_1.setPreferredSize(new Dimension(x, y));
+    pays_1.setPreferredSize(new Dimension(x, y));
+    jours_1.setPreferredSize(new Dimension(x, y));
+    mois_1.setPreferredSize(new Dimension(x, y));
+    anees_1.setPreferredSize(new Dimension(x, y));
+    
+	}
+}
